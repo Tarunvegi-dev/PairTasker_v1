091be9372117208d6c9525d7eebae1e2507f1add
TarunBabu <vegitarun1234@gmail.com>
2022-11-14 17:26:45 +0530

    sidedrawer completed

---
+++ lib/screens/home_screen/drawer.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 20,
+                ),
+                margin: const EdgeInsets.only(
+                  top: 75,
+                ),
+                color: Colors.white,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          InkWell(
+                            onTap: () => Scaffold.of(context).closeDrawer(),
+                            child: Icon(
+                              Icons.close,
+                              size: 30,
+                              color: HexColor('99A4AE'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const SizedBox(
+                        width: 60,
+                        height: 60,
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjdKRG2b-Z-wzIFPlATwulI-kKBy9LiKNcpAdBG4u5mf2X3aVPp2p62JYts9jz-1ABOnI&usqp=CAU'),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        'Will Smith',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        '@willsmith143',
+                        style: TextStyle(
+                          color: HexColor('AAABAB'),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ]),
+              ),
+              Container(
+                color: HexColor('E4ECF5'),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 27,
+                      ),
+                      margin: const EdgeInsets.only(
+                        bottom: 5,
+                        top: 5,
+                      ),
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset('assets/images/icons/drawer/profile.png'),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            'View Profile',
+                            style: TextStyle(
+                              color: HexColor('1A1E1F'),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 27,
+                      ),
+                      margin: const EdgeInsets.only(
+                        bottom: 5,
+                      ),
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                              'assets/images/icons/drawer/tasker_mode.png'),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            'Tasker Mode',
+                            style: TextStyle(
+                              color: HexColor('1A1E1F'),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20,
+                      ),
+                      margin: const EdgeInsets.only(
+                        bottom: 5,
+                      ),
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                              'assets/images/icons/drawer/terms-and-conditions.png'),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            'Terms & Conditions',
+                            style: TextStyle(
+                              color: HexColor('1A1E1F'),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20,
+                      ),
+                      margin: const EdgeInsets.only(
+                        bottom: 5,
+                      ),
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset('assets/images/icons/drawer/faq.png'),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "FAQ's",
+                            style: TextStyle(
+                              color: HexColor('1A1E1F'),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20,
+                      ),
+                      margin: const EdgeInsets.only(
+                        bottom: 5,
+                      ),
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset('assets/images/icons/drawer/share.png'),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            'Share the app',
+                            style: TextStyle(
+                              color: HexColor('1A1E1F'),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: HexColor('FF033E'),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                ),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(0),
+                  ),
+                ),
+              ),
+              child: const Text('Logout'),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pairtasker/screens/home_screen/drawer.dart';
 import 'package:pairtasker/screens/home_screen/taskers_list.dart';
 import 'package:pairtasker/theme/widgets.dart';
 import 'recents.dart';
@@ -19,134 +20,111 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: const DrawerWidget(),
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.black,
-                    width: 0.2,
+      body: Builder(
+        builder: (context) => SafeArea(
+          child: Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.black,
+                      width: 0.2,
+                    ),
                   ),
                 ),
-              ),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 15,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      InkWell(
-                        onTap: () => key.currentState?.openDrawer(),
-                        child: Container(
-                          width: 36,
-                          height: 36,
-                          margin: const EdgeInsets.only(
-                            right: 12,
-                          ),
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(125),
-                            ),
-                            color: Color.fromRGBO(0, 0, 0, 0.1),
-                          ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 15,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        InkWell(
+                          onTap: () => Scaffold.of(context).openDrawer(),
                           child: Container(
-                            padding: const EdgeInsets.all(10),
-                            child: InkWell(
-                              onTap: () => key.currentState?.openDrawer(),
-                              child: SvgPicture.asset(
-                                'assets/images/icons/menu.svg',
+                            width: 36,
+                            height: 36,
+                            margin: const EdgeInsets.only(
+                              right: 12,
+                            ),
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(125),
+                              ),
+                              color: Color.fromRGBO(0, 0, 0, 0.1),
+                            ),
+                            child: Container(
+                              padding: const EdgeInsets.all(10),
+                              child: InkWell(
+                                onTap: () => Scaffold.of(context).openDrawer(),
+                                child: SvgPicture.asset(
+                                  'assets/images/icons/menu.svg',
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.location_on_rounded,
-                            color: HexColor('#007FFF'),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on_rounded,
+                              color: HexColor('#007FFF'),
+                              size: 20,
+                            ),
+                            Text(
+                              'Guntur, AP',
+                              style: GoogleFonts.lato(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      child: InkWell(
+                        onTap: () =>
+                            Navigator.of(context).pushNamed('/searchscreen'),
+                        child: Container(
+                          width: 36,
+                          height: 36,
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(125),
+                            ),
+                            color: HexColor('007FFF'),
+                          ),
+                          child: const Icon(
+                            Icons.search_rounded,
+                            color: Colors.white,
                             size: 20,
                           ),
-                          Text(
-                            'Guntur, AP',
-                            style: GoogleFonts.lato(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    child: InkWell(
-                      onTap: () => Navigator.of(context).pushNamed('/searchscreen'),
-                      child: Container(
-                        width: 36,
-                        height: 36,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(125),
-                          ),
-                          color: HexColor('007FFF'),
-                        ),
-                        child: const Icon(
-                          Icons.search_rounded,
-                          color: Colors.white,
-                          size: 20,
                         ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: HexColor('#E4ECF5'),
-              ),
-              child: ListView(
-                shrinkWrap: true,
-                children: const [
-                  Recents(),
-                  TaskersList(),
-                ],
-              ),
-            )
-          ],
+              Container(
+                decoration: BoxDecoration(
+                  color: HexColor('#E4ECF5'),
+                ),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: const [
+                    Recents(),
+                    TaskersList(),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: const BottomNavBarWidget(0),
