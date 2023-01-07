58ffca1334886bf77665b0677b8e6b446fd5f335
Tarun Vegi <vegitarun1234@gmail.com>
2023-01-07 09:27:59 +0530

    FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,46 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:pairtasker/providers/taskers.dart';
 import 'screens/screens.dart';
 import 'providers/auth.dart';
 import 'screens/splash_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging.instance;
+    fbm.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    FirebaseMessaging.instance.getToken().then((value) => print(value));
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessaging);
+    super.initState();
+  }
+
+  Future<void> _firebaseMessaging(RemoteMessage message) async {
+    print(message);
+  }
 
   @override
   Widget build(BuildContext context) {
